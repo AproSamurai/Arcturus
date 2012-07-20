1dd3145f137520780ae4c656603a3ab0dd98f4bf
Timothy Lusk <darkcube@gmail.com>
2012-07-20 19:43:24 -0400

    vmscan: change shrinker API by passing shrink_control struct Change each shrinker's API by consolidating the existing parameters into shrink_control struct.  This will simplify any further features added w/o touching each file of shrinker.
    Change-Id: I215ebe935e265f2b8043c44156edf4d327f819cf

--- Kernel/drivers/gpu/drm/i915/i915_gem.c
+++ Kernel/drivers/gpu/drm/i915/i915_gem.c
@@ -4990,10 +4990,11 @@ i915_gpu_is_active(struct drm_device *dev)
 }
 
 static int
-i915_gem_shrink(struct shrinker *shrink, int nr_to_scan, gfp_t gfp_mask)
+i915_gem_shrink(struct shrinker *shrink, struct shrink_control *sc)
 {
 	drm_i915_private_t *dev_priv, *next_dev;
 	struct drm_i915_gem_object *obj_priv, *next_obj;
+	int nr_to_scan = sc->nr_to_scan;
 	int cnt = 0;
 	int would_deadlock = 1;
 
