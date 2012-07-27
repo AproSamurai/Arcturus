1b4b69d90d060b8cf0be94e0c9cb2d4cb64b1b37
Steven Smith <AproSamurai@gmail.com>
2012-07-26 22:57:06 -0700

    Change Customversion.

--- build_kernel.sh
+++ build_kernel.sh
@@ -17,7 +17,7 @@ DEFCONFIG_STRING=cyanogenmod_epicmtd_defconfig
 DEVICEPATH=device/samsung/epicmtd
 #TOOLCHAINPATH=/toolchain/arm-eabi-4.4.3/bin
 LOCALVERSION_STRING="-$CUSTOMVERSION"
-CUSTOMVERSION="Shuriken-2.0"
+CUSTOMVERSION="Arcturus"
 
 
 #TOOLCHAIN=`pwd`/toolchains/android-toolchain-4.4.3/bin
